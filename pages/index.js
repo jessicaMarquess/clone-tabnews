0561f9a56fd88ec72d7456160133f5dacd6136a5
jessicaMarquess <jessica.marques.dev@gmail.com>
2023-08-03 11:23:59 -0300

    feat: adicionando vida ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home () {
-    return <h1>hello world</h1>
+    return (
+        <div>
+        <h1>A vida fica sempre mais doce com uma bela música!</h1>
+        <a href='https://www.youtube.com/watch?v=LSgz7Ept79o' target='_blank'>Uma música maravilhosa para quem estiver acessando 😊</a>
+        </div>
+    )
 }
 
 export default Home
