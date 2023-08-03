644ddb90f4348d583ea739cd2d581355d3723a17
jessicaMarquess <jessica.marques.dev@gmail.com>
2023-08-03 11:24:05 -0300

    feat: adicionando vida ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home () {
     return (
         <div>
-        <h1>A vida fica sempre mais doce com uma bela música!</h1>
-        <a href='https://www.youtube.com/watch?v=LSgz7Ept79o' target='_blank'>Uma música maravilhosa para quem estiver acessando 😊</a>
+            <h1>A vida fica sempre mais doce com uma bela música!</h1>
+            <a href='https://www.youtube.com/watch?v=LSgz7Ept79o' target='_blank'>Uma música maravilhosa para quem estiver acessando 😊</a>
         </div>
     )
 }
