aa66a9e7249283bd83e62a7f5104944409b405e9
Jessica Marques <jessica.marques.dev@gmail.com>
2023-08-06 19:00:15 +0000

    adicionando emoji no fim da frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home () {
     return (
         <div>
-            <h1>A vida fica sempre mais doce com uma bela <a href="https://www.youtube.com/watch?v=erG5rgNYSdk">música</a>!</h1>
+            <h1>A vida fica sempre mais doce com uma bela <a href="https://www.youtube.com/watch?v=erG5rgNYSdk">música</a>! 🖤</h1>
         </div>
     )
 }
