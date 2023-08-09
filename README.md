ab41c4d99917c94166a1d4385c7280ef79902771
Jessica Marques <jessica.marques.dev@gmail.com>
2023-08-09 20:43:51 +0000

    feat: adicionando scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório feito a partir do curso de programação do curso.dev clonando o https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-function Home () {
-    return (
-        <div>
-            <h1>A vida fica sempre mais doce com uma bela <a href="https://www.youtube.com/watch?v=erG5rgNYSdk">música</a>! 🖤</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>
+        A vida fica sempre mais doce com uma bela{" "}
+        <a href="https://www.youtube.com/watch?v=erG5rgNYSdk">música</a>! 🖤
+      </h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
